692ee9156120569aa844d60e7a6868d396ba93e4
João Talone Gouveia <92531965+joaotgouveia@users.noreply.github.com>
2022-12-13 17:25:24 +0000

    Update generate_tests.py

--- stress_tests/generate_tests.py
+++ stress_tests/generate_tests.py
@@ -2,7 +2,7 @@ for s in range(5, 13):
     for i in range(s-1, s+1):
         with open(str(s)+"x"+str(i)+".in", "w") as test:
             test.write(str(s) + "\n")
-            test.write(str(s) + "\n")
+            test.write(str(i) + "\n")
 
-            for i in range(s):
+            for p in range(s):
                 test.write(str(i) + "\n")
